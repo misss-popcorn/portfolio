d614af2401179d90e8bf9fd070e3826ad64f0b82
Urvashi Soni <urvashi.soni05@gmail.com>
2020-10-04 15:04:00 +0530

    pre-commit config added

--- src/data/index.js
+++ src/data/index.js
@@ -1,51 +1,51 @@
 const getSocialMediaHandles = () => {
-    return [
-        {
-          "uid": "linkedIn",
-          "displayName": "Linked In",
-          "link": "https://www.linkedin.com/in/urvashi-soni-58262b56/",
-          "icon": "fa fa-linkedin-square",
-          "helpText": "Let's connect on LinkedIn"
-        },
-        {
-            "uid": "github",
-            "displayName": "Github",
-            "link": "https://github.com/usoni11",
-            "icon": "fa fa-github",
-            "helpText": "Github"
-          },
-          {
-            "uid": "stackOverflow",
-            "displayName": "Stack Overflow",
-            "link": "https://stackoverflow.com/users/2552408/urvashi",
-            "icon": "fa fa-stack-overflow",
-            "helpText": "Stack Overflow"
-          },
-        {
-          "uid": "facebook",
-          "displayName": "Facebook",
-          "link": "https://www.facebook.com/soniurvashii11/",
-          "icon": "fa fa-facebook-square",
-          "helpText": "Like us on Facebook"
-        },
-        {
-          "uid": "instagram",
-          "displayName": "Instagram",
-          "link": "https://www.instagram.com/misss_popcorn/",
-          "icon": "fa fa-instagram",
-          "helpText": "Let's follow each other in Instagram"
-        },
-        {
-          "uid": "twitter",
-          "displayName": "Twitter",
-          "link": "https://twitter.com/misss_popcorn",
-          "icon": "fa fa-twitter-square",
-          "helpText": "Follow us on Twitter"
-        }
+  return [
+    {
+      "uid": "linkedIn",
+      "displayName": "Linked In",
+      "link": "https://www.linkedin.com/in/urvashi-soni-58262b56/",
+      "icon": "fa fa-linkedin-square",
+      "helpText": "Let's connect on LinkedIn"
+    },
+    {
+      "uid": "github",
+      "displayName": "Github",
+      "link": "https://github.com/usoni11",
+      "icon": "fa fa-github",
+      "helpText": "Github"
+    },
+    {
+      "uid": "stackOverflow",
+      "displayName": "Stack Overflow",
+      "link": "https://stackoverflow.com/users/2552408/urvashi",
+      "icon": "fa fa-stack-overflow",
+      "helpText": "Stack Overflow"
+    },
+    {
+      "uid": "facebook",
+      "displayName": "Facebook",
+      "link": "https://www.facebook.com/soniurvashii11/",
+      "icon": "fa fa-facebook-square",
+      "helpText": "Like us on Facebook"
+    },
+    {
+      "uid": "instagram",
+      "displayName": "Instagram",
+      "link": "https://www.instagram.com/misss_popcorn/",
+      "icon": "fa fa-instagram",
+      "helpText": "Let's follow each other in Instagram"
+    },
+    {
+      "uid": "twitter",
+      "displayName": "Twitter",
+      "link": "https://twitter.com/misss_popcorn",
+      "icon": "fa fa-twitter-square",
+      "helpText": "Follow us on Twitter"
+    }
         
-      ]
+  ];
 };
 
 module.exports = {
-    getSocialMediaHandles
-}
+  getSocialMediaHandles
+};
